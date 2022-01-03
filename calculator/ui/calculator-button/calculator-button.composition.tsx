a2296b6e617db03ab7bfc69ae39a96adbf714706
Sunil Sandhu <sunil.sandhu87@gmail.com>
2022-01-03 19:17:52 +0000

    calculator-button wip

--- calculator/ui/calculator-button/calculator-button.composition.tsx
+++ calculator/ui/calculator-button/calculator-button.composition.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import CalculatorButton from './calculator-button';
 
-export const BasicCalculatorButton = () => (
-  <CalculatorButton text="hello from CalculatorButton" />
+export const BasicCalculatorNumberButton = () => (
+  <CalculatorButton text="1" type="number" />
+);
+
+export const BasicCalculatorFunctionButton = () => (
+  <CalculatorButton text="%" type="function" />
+);
+
+export const BasicCalculatorMemoryButton = () => (
+  <CalculatorButton text="M+" type="memory" />
+);
+
+export const BasicCalculatorEqualsButton = () => (
+  <CalculatorButton text="=" type="equals" />
 );
--- calculator/ui/calculator-button/calculator-button.tsx
+++ calculator/ui/calculator-button/calculator-button.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import Button from '@teambit/base-ui.input.button';
+import styles from './calculator-button.module.scss';
 import classNames from 'classnames';
 
 export type CalculatorButtonProps = {
   /**
    * a text to be rendered in the component.
    */
-  className?: string;
   text: string;
+  type: 'number' | 'function' | 'equals' | 'memory';
 };
 
 export default function CalculatorButton({
   text,
-  className,
+  type = 'number',
 }: CalculatorButtonProps) {
-  return <Button className={classNames(className)}>{text}</Button>;
+  return (
+    <Button className={classNames(styles.calculatorButton, styles[type])}>
+      {text}
+    </Button>
+  );
 }
